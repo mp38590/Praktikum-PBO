7fca58fd54213dd3e03751f56fca43cf1fac18b4
mp38590 <100133899+mp38590@users.noreply.github.com>
2023-03-08 19:47:16 +0700

    Add files via upload

---
+++ Pertemuan 3/direktori_anda/org/poligon/Poligon.java
@@ -0,0 +1,14 @@
+/**
+* File : Poligon.java 08/03/2023
+* Penulis : Mutiara Permata Putri
+* Deskripsi : representasi dasar dari objek poligon (segi banyak)
+*/
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
